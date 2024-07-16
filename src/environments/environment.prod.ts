44713f32099b5a3ea621109e14734b789a89f1c2
tongard <tongard.team@gmail.com>
2024-07-16 06:31:03 +0300

    update env endpont for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: "https://tongard.com",
+    baseUrl: "https://tongard.org",
     appNameText: 'follow my updates here: https://github.com/tongard'
 };
