0596f1b76c743763f6fc1612254b30782fdab7df
tongard <tongard.team@gmail.com>
2024-08-21 08:43:01 +0300

    pad

--- src/app/dashboards/gamepad/gamepad.component.ts
+++ src/app/dashboards/gamepad/gamepad.component.ts
@@ -63,7 +63,7 @@ export class GamepadComponent {
       zone: this.gamepadContainerRight.nativeElement, // Контейнер для джойстика
       dynamicPositionTarget: document.getElementById("root"),
       size: 90, // Размер джойстика
-      maxRange: 90,
+      maxRange: 140,
       threshold: 0.1, // Порог для минимального движения
       fadeTime: 250, // Время исчезновения джойстика при отсутствии активности
       color: 'blue', // Цвет джойстика
@@ -74,10 +74,10 @@ export class GamepadComponent {
     (obj: any) => {
       const currentTime = Date.now();
       if (currentTime - this.lastEmitTime > this.emitInterval) {
-        obj.x = obj.x + 90;
-        obj.y = obj.y + 90;
+        obj.x = obj.x + 140;
+        obj.y = obj.y + 140;
 
-        this.coordinates = {...this.coordinates, z:obj.y, d:obj.x}
+        this.coordinates = {...this.coordinates, z:obj.x, d:obj.y}
 
         this.socket.emit('move-arm', this.coordinates);
         this.lastEmitTime = currentTime;
