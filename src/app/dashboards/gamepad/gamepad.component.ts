b091463a5f6cdabd3c2ec83e44c509cf58237469
tongard <tongard.team@gmail.com>
2024-08-28 16:55:17 +0300

    update

--- src/app/dashboards/gamepad/gamepad.component.ts
+++ src/app/dashboards/gamepad/gamepad.component.ts
@@ -2,6 +2,8 @@ import { Component, AfterViewInit, ElementRef, ViewChild, ChangeDetectorRef } fr
 import { FormsModule } from '@angular/forms';
 // @ts-ignore
 import JoystickController from 'joystick-controller';
+// @ts-ignore
+import { Gamepad } from 'responsive-gamepad';
 import { CustomSocketService } from 'src/app/custom.socket.service';
 
 @Component({
@@ -12,8 +14,10 @@ import { CustomSocketService } from 'src/app/custom.socket.service';
   styleUrl: './gamepad.component.less'
 })
 export class GamepadComponent {
+  // @ViewChild('gamepadContainerLeft', { static: true }) gamepadContainerLeft!: ElementRef;
   @ViewChild('gamepadContainer', { static: true }) gamepadContainer!: ElementRef;
   @ViewChild('gamepadContainerRight', { static: true }) gamepadContainerRight!: ElementRef;
+  joystickLeft: any;
   joystick: any;
   joystickRight:any;
   private lastEmitTime: number = 0;
@@ -37,6 +41,30 @@ export class GamepadComponent {
   constructor(private socket: CustomSocketService, private cdref: ChangeDetectorRef) {}
 
   ngAfterViewInit() {
+
+    // this.joystickLeft = new Gamepad({
+    //   zone: this.gamepadContainerLeft.nativeElement, // Контейнер для джойстика
+    //   size: 90,
+    //   threshold: 0.1,
+    //   color: 'blue',
+    //   multitouch: true,
+    //   x: "20%",
+    //   y: "18%"
+    // });
+
+    // this.joystickLeft.on('move', (obj: any) => {
+    //   const currentTime = Date.now();
+    //   if (currentTime - this.lastEmitTime > this.emitInterval) {
+    //     obj.y = obj.y + 90;
+    //     obj.x = obj.x + 90;
+
+    //     this.coordinates = { ...this.coordinates, x: obj.y, y: 180 - obj.x };
+    //     this.socket.emit('move-arm', this.coordinates);
+    //     this.lastEmitTime = currentTime;
+    //     this.cdref.markForCheck();
+    //   }
+    // });
+
     this.joystick = new JoystickController({
       distortion:true,
       containerClass: "joystick-container",
@@ -44,21 +72,22 @@ export class GamepadComponent {
       joystickClass: "joystick",
       zone: this.gamepadContainer.nativeElement, // Контейнер для джойстика
       size: 90, // Размер джойстика
-      maxRange: 90,
+      maxRange: 180,
       threshold: 0.1, // Порог для минимального движения
       fadeTime: 250, // Время исчезновения джойстика при отсутствии активности
       color: 'blue', // Цвет джойстика
       multitouch: true, // Включение или отключение мультитача
-      x:"20%",
-      y:"18%"
+      x:"50%",
+      y:"30%"
     },
     (obj: any) => {
       const currentTime = Date.now();
       if (currentTime - this.lastEmitTime > this.emitInterval) {
-        obj.y = obj.y + 90;
-        obj.x = obj.x + 90;
-
-        this.coordinates = {...this.coordinates, x:obj.y, y:180-obj.x}
+        obj.y = obj.y;
+        obj.x = obj.x;
+        let rangeX = this.mapRange(obj.y, -180, 180, 40, 130)
+        let rangeY = this.mapRange(obj.x, -180, 180, 40, 130)
+        this.coordinates = {...this.coordinates, x:rangeX, y:rangeY}
         this.socket.emit('move-arm', this.coordinates);
         this.lastEmitTime = currentTime;
         this.cdref.markForCheck()
@@ -66,35 +95,35 @@ export class GamepadComponent {
     });
 
 
-    this.joystickRight = new JoystickController({
-      distortion:true,
-      containerClass: "joystick-container",
-      controllerClass: "joystick-controller",
-      joystickClass: "joystick",
-      zone: this.gamepadContainerRight.nativeElement, // Контейнер для джойстика
-      dynamicPositionTarget: document.getElementById("root"),
-      size: 90, // Размер джойстика
-      maxRange: 140,
-      threshold: 0.1, // Порог для минимального движения
-      fadeTime: 250, // Время исчезновения джойстика при отсутствии активности
-      color: 'blue', // Цвет джойстика
-      multitouch: true, // Включение или отключение мультитача
-      x:"80%",
-      y:"18%"
-    },
-    (obj: any) => {
-      const currentTime = Date.now();
-      if (currentTime - this.lastEmitTime > this.emitInterval) {
-        obj.x = obj.x + 140;
-        obj.y = obj.y + 140;
+    // this.joystickRight = new JoystickController({
+    //   distortion:true,
+    //   containerClass: "joystick-container",
+    //   controllerClass: "joystick-controller",
+    //   joystickClass: "joystick",
+    //   zone: this.gamepadContainerRight.nativeElement, // Контейнер для джойстика
+    //   dynamicPositionTarget: document.getElementById("root"),
+    //   size: 90, // Размер джойстика
+    //   maxRange: 140,
+    //   threshold: 0.1, // Порог для минимального движения
+    //   fadeTime: 250, // Время исчезновения джойстика при отсутствии активности
+    //   color: 'blue', // Цвет джойстика
+    //   multitouch: true, // Включение или отключение мультитача
+    //   x:"80%",
+    //   y:"18%"
+    // },
+    // (obj: any) => {
+    //   const currentTime = Date.now();
+    //   if (currentTime - this.lastEmitTime > this.emitInterval) {
+    //     obj.x = obj.x + 140;
+    //     obj.y = obj.y + 140;
 
-        this.coordinates = {...this.coordinates, z:this.mapRange(obj.x, 0, 180, 80, 100), d:140-obj.y}
+    //     this.coordinates = {...this.coordinates, z:this.mapRange(obj.x, 0, 180, 80, 100), d:140-obj.y}
 
-        this.socket.emit('move-arm', this.coordinates);
-        this.lastEmitTime = currentTime;
-        this.cdref.markForCheck()
-      }
-    });
+    //     this.socket.emit('move-arm', this.coordinates);
+    //     this.lastEmitTime = currentTime;
+    //     this.cdref.markForCheck()
+    //   }
+    // });
     
 
 
